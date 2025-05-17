971f7d698648c502d6d605a01c79a4d83fc8b70d
namsh1125 <namsh1125@naver.com>
2025-05-17 19:49:09 +0900

    Chore: mermaid 테마 설정 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -138,6 +138,11 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  themes: ['@docusaurus/theme-mermaid'],
+  markdown: {
+    mermaid: true,
+  }
 };
 
 export default config;
